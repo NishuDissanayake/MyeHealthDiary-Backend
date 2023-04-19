735cd90f9a20ba7981682f69ae4afd453527013a
NishuDissanayake <nishudissanayake5252@gmail.com>
2023-04-19 17:05:35 +0530

    All hospital operations completed

--- Routes/hospitalRoutes.js
+++ Routes/hospitalRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.post('/api/add-hospital', hospitalController.addHospital.bind(hospitalController));
 router.get('/api/get-hospitals', hospitalController.getHospitals.bind(hospitalController));
+router.put('/api/update-hospital-address', hospitalController.updateHospitalAddress.bind(hospitalController));
+router.put('/api/update-hospital-phone', hospitalController.updateHospitalContact.bind(hospitalController));
+router.put('/api/delete-hospital', hospitalController.deleteHospital.bind(hospitalController));
 
 module.exports = router;
