2695118be245a14dbc69a6032326537a56b79cdc
NishuDissanayake <nishudissanayake5252@gmail.com>
2023-04-19 15:14:32 +0530

    All admin operations successful

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -4,6 +4,9 @@ const adminController = require('./../controllers/adminController');
 const router = express.Router();
 
 router.post('/api/addadmin', adminController.addAdmin.bind(adminController));
-router.put('/api/putadminorg', adminController.putAdminOrg.bind(adminController));
-
+router.put('/api/updateadminorg', adminController.updateAdminOrg.bind(adminController));
+router.put('/api/updateadmindesignation', adminController.updateAdminPosition.bind(adminController));
+router.put('/api/updateadminphone', adminController.updateAdminPhone.bind(adminController));
+router.put('/api/updateadminpassword', adminController.updateAdminPassword.bind(adminController));
+router.put('/api/deleteadminacc', adminController.deactivateAdminAccount.bind(adminController));
 module.exports = router;
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -22,7 +22,7 @@ class adminController {
 
     }
 
-    async putAdminOrg(req, res) {
+    async updateAdminOrg(req, res) {
         try {
 
             const { _id, n_organization } = req.body;
@@ -33,7 +33,83 @@ class adminController {
                 { maxTimeMS: 30000 }
             ) 
 
-            res.status(200).json({ message: 'Updated successfully!' });
+            res.status(200).json({ message: 'Your organization updated successfully!' });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server Error');
+        }
+    }
+
+    async updateAdminPosition(req, res) {
+        try {
+
+            const { _id, n_designation } = req.body;
+
+            const result = await adminModel.findOneAndUpdate(
+                { _id: _id },
+                { designation: n_designation },
+                { maxTimeMS: 30000 }
+            ) 
+
+            res.status(200).json({ message: 'Your designation updated successfully!' });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server Error');
+        }
+    }
+
+    async updateAdminPhone(req, res) {
+        try {
+
+            const { _id, n_phone } = req.body;
+
+            const result = await adminModel.findOneAndUpdate(
+                { _id: _id },
+                { phone_number: n_phone },
+                { maxTimeMS: 30000 }
+            ) 
+
+            res.status(200).json({ message: 'Your phone number updated successfully!' });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server Error');
+        }
+    }
+
+    async updateAdminPassword(req, res) {
+        try {
+
+            const { _id, n_pass} = req.body;
+
+            const result = await adminModel.findOneAndUpdate(
+                { _id: _id },
+                { passwrd: n_pass },
+                { maxTimeMS: 30000 }
+            ) 
+
+            res.status(200).json({ message: 'Your password updated successfully!' });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server Error');
+        }
+    }
+
+    async deactivateAdminAccount(req, res) {
+        try {
+
+            const { _id, status} = req.body;
+
+            const result = await adminModel.findOneAndUpdate(
+                { _id: _id },
+                { status: status },
+                { maxTimeMS: 30000 }
+            ) 
+
+            res.status(200).json({ message: 'Account deactivated successfully!' });
 
         } catch (error) {
             console.error(error);
